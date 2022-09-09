035a91592cbcfac38ad7f6c54ee779c7d4a56156
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-09 11:23:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**main - Entry point
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+		printf("%d", ch);
+		printf("\n");
+
+	return (0);
+}
