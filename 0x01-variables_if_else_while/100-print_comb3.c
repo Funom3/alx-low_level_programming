f9b1d0c33e32ae9ca2fce19677462c1f207eb838
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-10 01:06:45 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,22 @@
 int main(void)
 {
 	int i;
+	int a;
 
-	for (i = 0; i < 90; i++)
+	for (i = 10; i <= 19; i++)
 	{
-		for ()
+		for (a = 10; a = <=19; a++)
 		{
-			putchar((i / 10) + 0);
-			putchar((i % 10) = 0);
-		}if (i != 89)
-		{
-			putchar(',');
-			putchar(' ');
+			if ((a % 10) > (i % 10))
+			{
+				putchar((i % 10) = '0');
+				putchar((a % 10) = '0');
+				if (i != 18 || a != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
