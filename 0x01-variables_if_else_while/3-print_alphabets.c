63422263e3be8edec8df55a2eee8c4636784d736
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-09 21:05:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,16 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: Always returns 0
+ * Return: Always 0
  */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <='z'; ch++);
+{
 		putchar(ch);
+}
 	for (ch = 'A'; ch <= 'Z'; ch++)
+{
 		putchar(ch);
+}
 	putchar('\n');
 	return (0);
 }
