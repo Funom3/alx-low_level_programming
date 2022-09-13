fc4d70e1fd5fefaf164acf67348776790c87e9f3
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-13 15:30:40 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	long int sum;
+	long int c1 = 0;
+	long int c2 = 1;
+	for (i = 1; i <= 50; i++)
+	{
+		sum = c1 + c2;
+		printf("%ld", sum);
+		c1 = c2;
+		c2 = sum;
+		if (i < 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
