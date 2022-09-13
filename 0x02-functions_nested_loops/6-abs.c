db131900f27d7de8256771b88c9040d86d885e57
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-13 12:29:52 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * abs - Begin here
+ *
+ * computes absolute values and returns the value
+ *
+ * @n: interger value
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
