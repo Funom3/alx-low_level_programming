e0fc32e8d8ae99f0b5fceadd61c6d2f120e2f424
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-13 15:40:34 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int f = 0;
+	long int c1 = 0;
+	long int c2 = 1;
+	long int sum = 0;
+	long int total = 0;
+
+	while (sum <= 4000000)
+	{
+		f = c1 + c2
+		total += f;
+		if (f % 2 == 0)
+			sum += f;
+		c1 = c2;
+		c2 = f;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
+
