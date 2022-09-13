3d9ab9ca33726ae7cdb60b65636eb82e31af89ce
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-13 11:53:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Begins here
+ *
+ * Return: 0 (true)
+ */
+int _islower(int c)
+
+{
+	if (c <= 'z' && c >= 'a')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
