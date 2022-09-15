ea8614067d4199d2376c17b040e3fcd3807c2cbd
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-15 12:38:58 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - highest prime numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int i, num;
+
+	num = 612852475143;
+	for (i = 2; i <= num; i++)
+	{
+		if (num % 1 == 0)
+		{
+			num = num / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
