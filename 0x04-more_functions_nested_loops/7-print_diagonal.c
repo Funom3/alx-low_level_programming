c46c25a69fdb59e6dfc8169cf31cd94891e548ff
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-15 10:32:45 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal line using backlashes
+ *
+ * @n: number of backlashes to use for printing
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; n < 0 && a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
