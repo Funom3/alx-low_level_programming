40b9ff35c16215b49ba22533f849f6f556131f50
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-19 11:00:45 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *reset_to_98 - Use pointer to change the value in a memory
+ * *@n: pointer to int n location
+ * *Return: Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
