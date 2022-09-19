b9f685d2010e2b4dd957d1a60d4506cb54b88348
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-19 11:57:10 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts_half - update value
+ *
+ * @str: value to evaluate
+ * 
+ * Return: void
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
