183afbacb59d9529a87840f9ecc0bdd440fc8666
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-19 11:35:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string location
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char rev[500];
+	int count;
+	
+	while (*s)
+	{
+		a++;
+		s++;
+	}
+	s--;
+	for (; > 0; a--)
+	{
+		rev[b] = *s;
+		s--;
+		b++;
+	}
+	s++;
+	count = b;
+	for (b = 0; b < count; b++)
+		*(s + b) = rev[b];
+}
