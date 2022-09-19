91314f6911bb097cd75381bf1547e2c35d9b6eb4
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-19 11:46:49 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of the string starting from the
+ * first
+ *
+ * @str: pointer to string location
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	len--;
+	for (z = 0; z <= len; z++)
+	{
+		if (z % 2 == 0)
+		{
+			putchar(str[z]);
+		}
+		putchar('\n');
+	}
+}
