7a733b4f364ac42a59b055f52f935baa5f199364
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-22 12:08:11 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,38 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, r = 0, lim;
+	while (*s1 && *s2 && s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	while (s1[a])
-	{
-		a++;
-	}
-	while (s2[a])
-	{
-		b++;
-	}
-	if (a <= b)
-	{
-		lim = a;
-	}
-	else
-	{
-		lim = b;
-	}
-	
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
-		{
-			c++;
-			continue;
-		}
-		else
-		{
-			r = s1[c] - s2[c];
-			break;
-		}
-		c++;
-	}
-	return (r);
+	return (*s1 - *s2);
 }
