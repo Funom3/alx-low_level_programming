ca966c0da8102b69891c609610345db5736fdc8d
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-09-28 10:48:24 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 	{
 		putchar('\n');
-		return;
 	}
-	putchar(*s);
-	_puts_recursion(s + 1);
 }
 
