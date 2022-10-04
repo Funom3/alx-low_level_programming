7a7062bee6b87588fa17bdb1aa07dda0272b9cad
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-04 11:41:49 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -41,7 +41,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (hgt_index = 0; hgt_index; height; hgt_index++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
 		for (wid_index = 0; wid_index < width; wid_index++)
 		{
