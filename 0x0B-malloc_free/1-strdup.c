07f6f99c06a3231010af0af39d2c8187592c5af3
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-04 10:28:47 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
+	char *arr;
 	int i;
 	len = 0;
 
@@ -25,15 +25,15 @@ char *_strdup(char *str)
 	{
 		len++;
 	}
-	duplicate = malloc(sizeof(char) * (len + 1));
+	arr = malloc(sizeof(char) * (i + 1));
 
-	if (duplicate == NULL)
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; str[i]; i++)
-		duplicate[i] = str[i];
-	duplicate[len] = '\0';
-	return (duplicate);
+	for (len = 0; str[len]; len++)
+		arr[len] = str[len];
+	arr[len] = '\0';
+	return (arr);
 }
