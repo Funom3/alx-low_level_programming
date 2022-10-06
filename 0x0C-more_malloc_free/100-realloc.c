4f1170bb5ea36c1daa38865f39ece87874a1c525
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-06 12:18:44 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *ptr_copy, *filter;
 	unsigned int i;
 
-	if (new_size == new_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
