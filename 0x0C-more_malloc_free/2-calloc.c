b0d7df53698f7e1d8cbbca22c3f989123c6783be
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-06 11:48:57 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memoryfor the array of the number
+ *
+ * @nmemb: the number of elements
+ *
+ * @size: the byte size of each array element
+ *
+ * Return: if nmemb 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	filler = mem;
+
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		filler[i] = '\0';
+	}
+	return (mem);
+}
