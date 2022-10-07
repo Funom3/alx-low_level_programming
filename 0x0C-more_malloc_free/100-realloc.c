bc3279bf234678c94e34bbc0eef8a20c776a2060
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-07 12:18:18 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * _realloc - reallocates a memory block using malloc
@@ -17,44 +19,36 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
 	char *ptr_copy, *filler;
 	unsigned int i;
 
+	if (ptr != NULL)
+	{
+		filler = ptr;
+	}
+	else
+	{
+		return (malloc(new_size));
+	}
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-
-		if (mem == NULL)
-		{
-			return (NULL);
-		}
-		return (mem);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
-	ptr_copy = ptr;
-	mem - malloc(sizeof(*ptr_copy) * new_size);
-
-	if (mem == NULL)
+	ptr_copy = malloc(new_size);
+	if (ptr_copy == NULL)
 	{
-		free(ptr);
-		return (NULL);
+		return (0);
 	}
-	filler = mem;
 
-	for (i = 0; i < old_size && i < new_size; i++)
+	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		filler[i] = *ptr_copy++;
+		*(ptr_copy + 1) = filler[i];
 	}
 	free(ptr);
-	return (mem);
+	return (filler);
 }
