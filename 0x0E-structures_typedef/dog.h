313294871d2c72d0176d3e1ae8c1d82a35867ed3
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-10 10:03:10 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,30 +8,31 @@
  */
 
 /**
- * struc dog - a new type describing a dog
+ * struct dog - a new type describing a dog
  *
  * @name: the name of the dog
  *
  * @age: the age of the dog
  *
- *
  * @owner: the owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
 /**
- * dog_t - typedef for struct dog
+ * dog_t - typedef dog
  */
-typedef struct dog dog_t;
+dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
