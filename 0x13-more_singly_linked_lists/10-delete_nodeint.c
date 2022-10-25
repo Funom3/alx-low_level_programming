34d110e3ee3936af5daf59e5553929ab78a7d7e6
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-25 07:30:35 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete the node at a given index of
+ * a listint_t list
+ *
+ * @index: the index of the node to be deleted
+ *
+ * @head: the pointer to the address of the head of the listint_t
+ *
+ * Return: success 1, failure -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *copy = *head;
+	unsigned int node;
+
+	if (copy == NULL)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next == NULL)
+		{
+			return (-1);
+		}
+		copy = copy->next;
+	}
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -34,6 +34,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		{
 			last = last->next;
 		}
+		last->next = new;
 	}
 	return (*head);
 }
