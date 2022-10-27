429c1ec9107946804ff67dd092dbcf8b438e172f
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-27 12:50:58 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ *
+ * @b: a pointer to a string
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int n;
+
+	n = 0;
+	if (!b)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n <<= 1;
+		if (b[i] == '1')
+		{
+			n += 1;
+		}
+	}
+	return (n);
+}
