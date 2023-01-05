9783039dc5d1cb3fb6a196fd66bc337b46978200
Funom# <kingsleyisaackushang1997@gmail.com>
2023-01-05 15:02:57 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,22 +10,24 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *tmp, *cursor;
+	hash_node_t *tmp, *node;
+	hash_table_t *head = ht;
 
-	if (!ht)
-		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		cursor = ht->array[i];
-		while (cursor)
+		if (ht->array[i] != NULL)
 		{
-			tmp = cursor->next;
-			free(cursor->key);
-			free(cursor->value);
-			free(cursor);
-			cursor = tmp;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
